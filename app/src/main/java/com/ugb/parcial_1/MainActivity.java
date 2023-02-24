d1e007447726f8dd8b2bfb74503c5ab16efe0c9d
DIEGO ENMANUEL ZEPEDA RENDEROS <USSS017922@ugb.edu.sv>
2023-02-24 11:17:35 -0600

    Conversor

---
+++ app/src/main/java/com/ugb/parcial_1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.ugb.parcial_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView temp;
+    Spinner spn;
+    conversores miConversor = new conversores();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btnConvertir);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp = (TextView) findViewById(R.id.txtcantidad);
+                double cantidad = Double.parseDouble(temp.getText().toString());
+
+                spn = findViewById(R.id.spnde);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spna);
+                int a = spn.getSelectedItemPosition();
+
+                temp = findViewById(R.id.lblrespuesta);
+                temp.setText("Respuesta: "+ miConversor.convertir(0, de, a, cantidad));
+            }
+        });
+    }
+}
+class conversores{
+    double[][] valores = {
+            {0.698896, 1e-6, 628.86, 0.70, 10000, 0.000247105,},
+
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
+    }
+}
